ffc2766d58f457e2c5defca4c6ab1a9bbcb93722
apfleegor <apfleegor@oxy.edu>
2022-12-06 00:13:22 -0500

    deleted data where there was only one coordinate

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,24 +1,9 @@
-// function getOccurrence(array, value) {
-//     var count = 0;
-//     array.forEach((v) => (v === value && count++));
-//     return count;
-// };
-// function getAllIndexes(arr, val) {
-//     var indexes = [], i;
-//     for(i = 0; i < arr.length; i++)
-//         if (arr[i] === val)
-//             indexes.push(i);
-//     return indexes;
-// };
+
 function demoInfo(hurricane) {
     var ol = document.getElementById('metalist');
     var listy = document.getElementById('metalist');
     listy.innerHTML = '';
-    // for (let i = 0; i < keys.length -1; i++) {
-    //     let li = document.createElement('li');
-    //     li.innerText=`${keys[i]}: ${values[i][0]}`;
-    //     ol.appendChild(li);
-    // };
+
     let li = document.createElement('li');
     li.innerText=`Name: ${hurricane.name}`;
     ol.appendChild(li);
@@ -37,14 +22,6 @@ function demoInfo(hurricane) {
 };
 
 
-// function categories(categorydata) {
-//     var category = Object.values(categorydata.Category);
-//     var year = Object.values(categorydata.Year);
-    
-//     let trace6 = {
-        
-//     }
-// }
 
 function get_scraped_img(id) {
     for (var i=0; i<Object.keys(scraped.id).length; i++) {
